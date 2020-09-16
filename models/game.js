6b77db50569aed35c59ef9c115e5c9bb52ef2fb1
khareesmith <kharee.smith@yahoo.com>
2020-09-15 21:05:11 -0400

    added a route for posting game. Updated middleware for finding games by ID

--- models/game.js
+++ models/game.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 
 // Sets basic Schema for games that takes in properties for name, developer, publisher, platforms, release date, genre, description of game, and image of game.
 const gamesSchema = new mongoose.Schema({
+    _id: {type: String},
     name: {type: String, required: true},
     developer: {type: String, required: true},
     publisher: {type: String, required: true},
     platforms: [{type: String, name: String, required: true}],
     release_date: {type: Date, default: Date.now},
     genre: {type: String, required: true},
-    description: {type: String, required: true},
-    img: {type: Buffer, contentType: String}
+    description: {type: String, required: true}
 })
 
 module.exports = mongoose.model('Game', gamesSchema)
--- routes/games.js
+++ routes/games.js
@@ -13,9 +13,51 @@ router.get('/', async (req, res) => {
 })
 
 // Route to get one game on the server
-router.get('/:id', (req, res) => {
-    res.send(req.params)
+router.get('/:id', getGameId, async (req, res) => {
+    try {
+    const one_game = await games.findById(req.params.id)
+    res.json(one_game)
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
+// Route to add game to server
+router.post('/', async (req, res) => {
+    const game = new games({
+        _id: req.body.name,
+        name: req.body.name,
+        developer: req.body.developer,
+        publisher: req.body.publisher,
+        platforms: [req.body.platforms],
+        genre: req.body.genre,
+        description: req.body.description
+    })
+    try {
+        const newGame = await game.save()
+        res.status(201).json(newGame)
+    }
+    catch (err) {
+    res.status(501).json({message: err.message})
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+
+})
+
+async function getGameId(req, res, next) {
+    let gameId
+    try {
+        gameId = await games.findById(req.params.id)
+        if (gameId == null) {
+            res.status(404).json({message: 'Could not find game by name'})
+        }
+    } catch (err) {
+
+    }
+    res.id = gameId
+    next()
+}
 
 module.exports = router
