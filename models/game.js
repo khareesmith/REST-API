19ff2cca4bf6cb5a5f6dabe5494d3b0354d1f77e
khareesmith <kharee.smith@yahoo.com>
2020-09-18 19:02:20 -0400

    added route for deleting games

--- models/game.js
+++ models/game.js
@@ -2,11 +2,10 @@ const mongoose = require('mongoose')
 
 // Sets basic Schema for games that takes in properties for name, developer, publisher, platforms, release date, genre, description of game, and image of game.
 const gamesSchema = new mongoose.Schema({
-    _id: {type: String},
     name: {type: String, required: true},
     developer: {type: String, required: true},
     publisher: {type: String, required: true},
-    platforms: [{type: String, name: String, required: true}],
+    platforms: {type: Array, name: String, required: true},
     release_date: {type: Date, default: Date.now},
     genre: {type: String, required: true},
     description: {type: String, required: true}
--- routes/games.js
+++ routes/games.js
@@ -1,11 +1,13 @@
+const { json } = require('express')
 const express = require('express')
+const { remove } = require('../models/game')
 const router = express.Router()
-const games = require('../models/game')
+const Games = require('../models/game')
 
 // Route to get all games on the server
 router.get('/', async (req, res) => {
     try {
-        const allgames = await games.find()
+        const allgames = await Games.find()
         res.json(allgames)
     } catch (err) {
         res.status(500).json({message: err.message})
@@ -13,10 +15,9 @@ router.get('/', async (req, res) => {
 })
 
 // Route to get one game on the server
-router.get('/:id', getGameId, async (req, res) => {
+router.get('/:id', getGame, async (req, res) => {
     try {
-    const one_game = await games.findById(req.params.id)
-    res.json(one_game)
+    res.send(res.game)
     } catch (err) {
         res.status(500).json({message: err.message})
     }
@@ -24,12 +25,12 @@ router.get('/:id', getGameId, async (req, res) => {
 
 // Route to add game to server
 router.post('/', async (req, res) => {
-    const game = new games({
-        _id: req.body.name,
+    const game = new Games({
         name: req.body.name,
         developer: req.body.developer,
         publisher: req.body.publisher,
         platforms: [req.body.platforms],
+        release_date: req.body.release_date,
         genre: req.body.genre,
         description: req.body.description
     })
@@ -42,21 +43,31 @@ router.post('/', async (req, res) => {
     }
 })
 
-router.delete('/:id', async (req, res) => {
-
+// Route to delete game from server by id
+router.delete('/:id',getGame, async (req, res) => {
+    try {
+        await res.game.remove()
+        res.json({message: "Game removed"})
+    } catch (err) {
+        res.status(500).json({message: err.message})
+    }
 })
 
-async function getGameId(req, res, next) {
-    let gameId
+// Route to update a game
+
+// Separate function to get a single game by it's id in the document. This game object is passed as middleware for REST functions.
+async function getGame(req, res, next) {
+    let game
+    let id = req.params.id
     try {
-        gameId = await games.findById(req.params.id)
-        if (gameId == null) {
-            res.status(404).json({message: 'Could not find game by name'})
+        game = await Games.findById(id)
+        if (game == null) {
+            return res.status(404).json({message: 'Could not find game by name'})
         }
     } catch (err) {
-
+        res.status(500).json(err.message)
     }
-    res.id = gameId
+    res.game = game
     next()
 }
 
